6a4bee32210ef93c4ee7d68f27d482650fce8d15
friend-of-moon <phucan0147@gmail.com>
2025-02-18 14:03:00 +0700

    update register page and update language for login and register page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import CustomerLayout from "./Layout/CustomerLayout";
 import HomePage from "./Pages/CustormerPages/HomePage/HomePage";
 import LoginPage from "./Pages/LoginRegister/LoginPage/Login";
 import BlogPage from "./Pages/CustormerPages/Blogger/BlogPage";
+import Register from "./Pages/LoginRegister/RegisterPage/Register";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<CustomerLayout />}>
           <Route index element={<HomePage />}></Route>
           <Route path="/login" element={<LoginPage />}></Route>
+          <Route path="/register" element={<Register />}></Route>
           <Route path="/blogger" element={<BlogPage />}></Route>
           {/* Đúng tên */}
         </Route>
--- src/Pages/LoginRegister/LoginPage/Login.jsx
+++ src/Pages/LoginRegister/LoginPage/Login.jsx
@@ -1,81 +1,75 @@
-import React from 'react'
 import "./Login.css";
-import logo from "../../../assets/images/logo.png"
-import iconGG from "../../../assets/login/iconGG.png"
-import { Button } from 'antd';
-import { Link } from 'react-router-dom';
+import logo from "../../../assets/images/logo.png";
+import iconGG from "../../../assets/login/iconGG.png";
+import { Button } from "antd";
+import { Link } from "react-router-dom";
 
 function LoginPage() {
   return (
-    <div className='login-container'>
-
-      <div className='login-left'>
-        <div className='login-logo'>
-          <img src={logo} alt='logo page' />
+    <div className="login-container">
+      <div className="login-left">
+        <div className="login-logo">
+          <img src={logo} alt="logo page" />
         </div>
 
-        <p style={{color: "white"}}>
-          Your skin is your bodys largest organ, and it deserves all the care
-          and love you can give.
+        <p style={{ color: "white" }}>
+          Làn da của bạn là cơ quan lớn nhất của cơ thể và nó xứng đáng nhận
+          được mọi sự chăm sóc và yêu thương mà bạn có thể dành cho.
         </p>
-        <Link >
-          <Button>Sign Up</Button>
+        <Link to={"/register"}>
+          <Button style={{ marginTop: "20px" }}>Đăng ký</Button>
         </Link>
       </div>
 
       {/* LOGIN RIGHT***************** */}
-      <div className='login-right'>
+      <div className="login-right">
         <h2>Đăng nhập</h2>
         <form>
-          <div className='form-group'>
+          <div className="form-group">
             <label>Email</label>
             <input
               type="email"
               name="email"
-              placeholder='Enter your email...'
+              placeholder="Nhập email của bạn..."
               required
             />
           </div>
 
-          <div className='form-group'>
-            <label>Password</label>
+          <div className="form-group">
+            <label>Mật khẩu</label>
             <input
               type="password"
               name="password"
-              placeholder='**********'
+              placeholder="**********"
               required
             />
           </div>
 
-          <div className='form-options'>
+          <div className="form-options">
             <label>
-              <input type="checkbox" />
-              Keep me login!!!
+              <input type="checkbox" style={{ marginRight: "5px" }} />
+              Giữ đăng nhập lần sau!!!
             </label>
-            <Link>Forgot password</Link>
+            <Link>Quên mật khẩu</Link>
           </div>
-          <button
-            type='submit'
-            className='login-button'
-
-          />
+          <button type="submit" className="login-button">
+            Đăng nhập
+          </button>
         </form>
 
-        <p style={{ marginBottom: "-5px" }}>Or login with google</p>
-        <div className='social-login'>
+        <p style={{ marginBottom: "-5px" }}>Hoặc đăng nhập với Google</p>
+        <div className="social-login">
           <button className="google">
             <img src={iconGG} alt="" />
           </button>
         </div>
 
         <p>
-          Don't have an account? <a href="">Register</a>
+          Không có tài không? <Link to="/register">Đăng ký</Link>
         </p>
-
       </div>
-
     </div>
-  )
+  );
 }
 
 export default LoginPage;
