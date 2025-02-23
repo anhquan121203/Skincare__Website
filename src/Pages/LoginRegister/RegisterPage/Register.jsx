f9f9a01d01e3ecfd8fbc8e2b58157f9251006e15
friend-of-moon <phucan0147@gmail.com>
2025-02-23 20:11:28 +0700

    logic resgiter

--- src/Pages/LoginRegister/RegisterPage/Register.jsx
+++ src/Pages/LoginRegister/RegisterPage/Register.jsx
@@ -1,17 +1,68 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Logo from "../../../assets/images/logo.png";
 import "./Register.css";
+import { useDispatch } from "react-redux";
+import { useFormik } from "formik";
+import { registerUser } from "../../../Api/authApi";
+import { toast } from "react-toastify";
+import { login } from "../../../Features/user/authSlice";
+import * as Yup from "yup";
 
 function Register() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  // Validate schema
+  const validationSchema = Yup.object({
+    firstName: Yup.string().required("Tên phải bắt buộc"),
+    lastName: Yup.string().required("Họ phải bắt buộc"),
+    birthday: Yup.date().required("Ngày sinh phải bắt buộc"),
+    phoneNumber: Yup.string()
+      .matches(/^\d+$/, "Phone must be a valid number")
+      .required("Số điện thoại bắt buộc nhập"),
+    address: Yup.string().required("Địa chỉ bắt buộc nhập"),
+    email: Yup.string().required("Email is required"),
+    password: Yup.string().required("Password is required"),
+    confirmPassword: Yup.string()
+      .oneOf([Yup.ref("password"), null], "Passwords must match")
+      .required("Confirm password is required"),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      birthday: "",
+      phoneNumber: "",
+      address: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+    validationSchema,
+    onSubmit: async (values) => {
+      try {
+        const response = await registerUser(values);
+        if (response.status === 200) {
+          toast.success("Đăng kí thành công!!!");
+          const { accessToken, refreshToken } = response.data;
+
+          localStorage.setItem("accessToken", accessToken);
+          localStorage.setItem("refreshToken", refreshToken);
+
+          dispatch(login({ accessToken, refreshToken }));
+          navigate("/login");
+        } else {
+          toast.error(response.data.message || "Đăng kí thất bại!!!");
+        }
+      } catch (error) {
+        toast.error("An error occurred during registration");
+      }
+    },
+  });
+
   return (
     <div className="register-container">
-      {/* <div className="video-background">
-                <video autoPlay loop muted className="background-video">
-                    <source src="src/assets/image login/video6.mp4" type="video/mp4" />
-                    Your browser does not support the video tag.
-                </video>
-            </div> */}
-
       <div className="register-left">
         <div className="register-logo">
           <img src={Logo} alt="Logo" />
@@ -27,7 +78,7 @@ function Register() {
       <div className="register-right">
         <h2>Đăng ký</h2>
         <p>Tạo tài khoản mới của bạn</p>
-        <form className="form-register">
+        <form className="form-register" onSubmit={formik.handleSubmit}>
           <div className="form-row-register">
             <div className="form-group-register">
               <label>Họ</label>
@@ -35,6 +86,14 @@ function Register() {
                 type="text"
                 name="firstName"
                 placeholder="Họ..."
+                value={formik.values.firstName}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                className={
+                  formik.touched.firstName && formik.errors.firstName
+                    ? "error"
+                    : ""
+                }
                 required
               />
             </div>
@@ -45,6 +104,14 @@ function Register() {
                 type="text"
                 name="lastName"
                 placeholder="Tên..."
+                value={formik.values.lastName}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                className={
+                  formik.touched.lastName && formik.errors.lastName
+                    ? "error"
+                    : ""
+                }
                 style={{ width: "190px" }}
                 required
               />
@@ -57,6 +124,14 @@ function Register() {
               <input
                 type="date"
                 name="birthday"
+                value={formik.values.birthday}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                className={
+                  formik.touched.birthday && formik.errors.birthday
+                    ? "error"
+                    : ""
+                }
                 required
                 style={{ maxWidth: "400px" }}
               />
@@ -66,7 +141,15 @@ function Register() {
               <label>Số điện thoại</label>
               <input
                 type="text"
-                name="phone"
+                name="phoneNumber"
+                value={formik.values.phoneNumber}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                className={
+                  formik.touched.phoneNumber && formik.errors.phoneNumber
+                    ? "error"
+                    : ""
+                }
                 placeholder="Số điện thoại của bạn..."
                 required
               />
@@ -79,6 +162,12 @@ function Register() {
               type="text"
               name="address"
               placeholder="Nhập địa chỉ của bạn..."
+              value={formik.values.address}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              className={
+                formik.touched.address && formik.errors.address ? "error" : ""
+              }
               required
             />
           </div>
@@ -89,6 +178,12 @@ function Register() {
               type="email"
               name="email"
               placeholder="demo@gmail.com"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              className={
+                formik.touched.email && formik.errors.email ? "error" : ""
+              }
               required
             />
           </div>
@@ -100,6 +195,14 @@ function Register() {
                 type="password"
                 name="password"
                 placeholder="********"
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                className={
+                  formik.touched.password && formik.errors.password
+                    ? "error"
+                    : ""
+                }
                 required
               />
             </div>
@@ -110,13 +213,26 @@ function Register() {
                 type="password"
                 name="confirmPassword"
                 placeholder="********"
+                value={formik.values.confirmPassword}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                className={
+                  formik.touched.confirmPassword &&
+                  formik.errors.confirmPassword
+                    ? "error"
+                    : ""
+                }
                 required
               />
             </div>
           </div>
 
-          <button type="submit" className="register-button">
-            Đăng ký
+          <button
+            type="submit"
+            className="register-button"
+            disabled={formik.isSubmitting}
+          >
+            {formik.isSubmitting ? "Đăngkí..." : "Đăng kí"}
           </button>
         </form>
 
