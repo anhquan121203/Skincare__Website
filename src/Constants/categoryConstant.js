63b6695836b5523be4f9ce66e3f75ae24f8d89fc
friend-of-moon <phucan0147@gmail.com>
2025-03-01 21:35:00 +0700

    update api category

---
+++ src/Constants/categoryConstant.js
@@ -0,0 +1,2 @@
+export const CATEGORY_API_URL = "https://localhost:7088/api";
+export const CATEGORY = "category";
--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "../Features/user/authSlice";
 import orderSlice from "../Features/order/orderSlice";
+import categorySlice from "../Features/category/categorySlice";
 
 export const store = configureStore({
   reducer: {
     auth: authSlice.reducer,
     order: orderSlice.reducer,
+    category: categorySlice.reducer,
   },
 });
