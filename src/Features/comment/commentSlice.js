0bb509fd1e7f8e8eee92332108c260068233fb9d
friend-of-moon <phucan0147@gmail.com>
2025-03-05 20:45:19 +0700

    commit

--- src/Features/comment/commentSlice.js
+++ src/Features/comment/commentSlice.js
@@ -1,43 +1,54 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
-import { COMMENT_API_URL, COMMENT } from '../../Constants/commentConstant';
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import { COMMENT_API_URL, COMMENT } from "../../Constants/commentConstant";
 
-
-export const fetchComments = createAsyncThunk('comment/fetchCommnet', async (_, { rejectWithValue }) => {
-  try {
-    const response = await axios.get(COMMENT_API_URL);
-    return response.data;
-  } catch (error) {
-    return rejectWithValue(error.response?.data || error.message);
+export const fetchComments = createAsyncThunk(
+  "comment/fetchCommnet",
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await axios.get(COMMENT_API_URL);
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.response?.data || error.message);
+    }
   }
-});
+);
 
-export const createComment = createAsyncThunk('comment/createComment', async (comment, { rejectWithValue }) => {
-  try {
-    const response = await axios.post(COMMENT_API_URL, comment);
-    return response.data;
-  } catch (error) {
-    return rejectWithValue(error.response?.data || error.message);
+export const createComment = createAsyncThunk(
+  "comment/createComment",
+  async (comment, { rejectWithValue }) => {
+    try {
+      const response = await axios.post(COMMENT_API_URL, comment);
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.response?.data || error.message);
+    }
   }
-});
+);
 
-export const updateComment = createAsyncThunk('comment/updateComment', async ({ id, comment }, { rejectWithValue }) => {
-  try {
-    const response = await axios.put(`${COMMENT_API_URL}/${id}`, comment);
-    return response.data;
-  } catch (error) {
-    return rejectWithValue(error.response?.data || error.message);
+export const updateComment = createAsyncThunk(
+  "comment/updateComment",
+  async ({ id, comment }, { rejectWithValue }) => {
+    try {
+      const response = await axios.put(`${COMMENT_API_URL}/${id}`, comment);
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.response?.data || error.message);
+    }
   }
-});
+);
 
-export const removeComment = createAsyncThunk('comment/removeComment', async (id, { rejectWithValue }) => {
-  try {
-    await axios.delete(`${COMMENT_API_URL}/${id}`);
-    return id;
-  } catch (error) {
-    return rejectWithValue(error.response?.data || error.message);
+export const removeComment = createAsyncThunk(
+  "comment/removeComment",
+  async (id, { rejectWithValue }) => {
+    try {
+      await axios.delete(`${COMMENT_API_URL}/${id}`);
+      return id;
+    } catch (error) {
+      return rejectWithValue(error.response?.data || error.message);
+    }
   }
-});
+);
 
 const commentSlice = createSlice({
   name: COMMENT,
@@ -65,13 +76,15 @@ const commentSlice = createSlice({
         state.comments.push(action.payload);
       })
       .addCase(updateComment.fulfilled, (state, action) => {
-        const index = state.comments.findIndex(p => p.id === action.payload.id);
+        const index = state.comments.findIndex(
+          (p) => p.id === action.payload.id
+        );
         if (index !== -1) {
           state.comments[index] = action.payload;
         }
       })
       .addCase(removeComment.fulfilled, (state, action) => {
-        state.comments = state.comments.filter(p => p.id !== action.payload);
+        state.comments = state.comments.filter((p) => p.id !== action.payload);
       });
   },
 });
